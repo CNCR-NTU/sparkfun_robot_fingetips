1990adca746be9d62c70aaacd66938fd86280f13
Pedro Machado <pedro.baptistamachado@ntu.ac.uk>
2019-09-26 18:45:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-	packages=['soarkfun_robot_fingertips'],
+	packages=['sparkfun_robot_fingertips'],
 	package_dir={'':'scripts'},
 )
 
